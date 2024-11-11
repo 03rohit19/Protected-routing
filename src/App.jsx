c13d7f88b608af2ac04dd554d66651e2424c7bf7
Rohit <rohitfriday01@gmail.com>
2024-11-11 13:53:03 +0530

    Adding App.jsx file

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RoutePage from "./Router";
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <RoutePage />
+    </div>
+  );
+}
+
+export default App;
