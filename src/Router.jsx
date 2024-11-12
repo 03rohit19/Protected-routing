cde15587381ac6d8a5733c2dbc260d91c2cbf81e
Rohit <rohitfriday01@gmail.com>
2024-11-12 21:59:38 +0530

    Adding updated file

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,10 @@
-// RoutePage.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import About from "./components/About";
@@ -8,22 +12,35 @@ import Subscribe from "./components/Subscribe";
 import Navbar from "./components/Navbar";
 import ProtectedRoute from "./components/ProtectedRoute";
 import HomePage from "./components/HomePage";
+import { useSelector } from "react-redux"; // Import useSelector to access the state
 
 const RoutePage = () => {
+  const { isLoggedIn } = useSelector((state) => state.user); // Assuming the state contains these values
+
+  console.log("isLoggedIn value:", isLoggedIn); // This will log the value of isLoggedIn
+
   return (
     <Router>
-      <Navbar />
+      <Navbar isLoggedIn={isLoggedIn} />
       <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-
-        {/* ProtectedRoute */}
-        <Route element={<ProtectedRoute />}>
-          <Route path="/subscribe" element={<Subscribe />} />
-          <Route path="/homepage" element={<HomePage />} />
-        </Route>
+        {/* Unauthorized routes */}
+        {!isLoggedIn ? (
+          <>
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/" element={<SignIn />} />
+          </>
+        ) : (
+          <Route element={<ProtectedRoute />}>
+            <Route path="/signin" element={<Navigate to="/" />} />
+            <Route path="/signup" element={<Navigate to="/" />} />
+            <Route path="/subscribe" element={<Subscribe />} />
+            <Route path="/" element={<HomePage />} />
+          </Route>
+        )}
 
         <Route path="/about" element={<About />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
