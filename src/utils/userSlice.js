a7549ea25c159161f019b647f7b2df66bfa25623
Rohit <rohitfriday01@gmail.com>
2024-11-12 22:09:20 +0530

    Adding updated file

--- src/utils/userSlice.js
+++ src/utils/userSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   user: null, // To store user data (name, email, etc.)
+  isLoggedIn: false, // To track if the user is logged in
 };
 
 const userSlice = createSlice({
@@ -11,9 +12,11 @@ const userSlice = createSlice({
   reducers: {
     setUser: (state, action) => {
       state.user = action.payload; // Set the user data
+      state.isLoggedIn = true; // Set logged-in status to true
     },
     removeUser: (state) => {
       state.user = null; // Remove user data on logout
+      state.isLoggedIn = false; // Set logged-in status to false
     },
   },
 });
